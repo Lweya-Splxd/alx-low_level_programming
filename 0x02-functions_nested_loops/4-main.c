b24182006e3defcadae58b65a23783fea3f87cbe
Lweya-Splxd <elvislweya@gmail.com>
2023-02-21 13:49:51 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
-* _isalpha - checks for alphabets
-* @c: character to be checked
-*
-* Return:1 if character is a letter, 0 otherwise
-*/
-
-int _isalpha(int c)
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return (1);
-else
-return (0);
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
+
